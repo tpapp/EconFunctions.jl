cc041faf1c387d9b299ff3912db059f1686fded7
Tamas K. Papp <tkpapp@gmail.com>
2017-03-27 20:40:21 +0200

    Added missed file.

---
+++ src/quadrature.jl
@@ -0,0 +1,38 @@
+using ContinuousTransformations
+import ContinuousTransformations: domain
+using FastGaussQuadrature
+
+export Quadrature, quadrature_standard_normal, quadrature_standard_uniform
+
+"""
+Univariate numerical quadrature. A lightweight container for the
+`nodes` and the `weights`, calculated from some quadrature rule. The
+`domain` is provided for information purposes.
+"""
+immutable Quadrature{TD <: AbstractInterval, T}
+    domain::TD
+    nodes::Vector{T}
+    weights::Vector{T}
+end
+
+domain(q::Quadrature) = q.domain
+
+(q::Quadrature)(f) = dot(f.(q.nodes), q.weights)
+
+"""
+Return a quadrature that integrates a function under the standard
+normal distribution using `N` nodes.
+"""
+function quadrature_standard_normal(N::Int)
+    nodes, weights = gausshermite(N)
+    Quadrature(ℝ, nodes*√2, weights/√π)
+end
+
+"""
+Return a quadrature that integrates a function on the unit domain
+[0,1] under the standard uniform distribution using `N` nodes.
+"""
+function quadrature_standard_uniform(N::Int)
+    nodes, weights = gausslegendre(N)
+    Quadrature(𝕀, fma.(nodes, 0.5, 0.5), weights .* 0.5)
+end
